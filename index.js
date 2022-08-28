c2de233377921a7639dcd5310e013c6b55453434
Leo <leomutungi19@gmail.com>
2022-08-28 12:55:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayOfLetters = word.split("");
+	const reverseLetters = arrayOfLetters.reverse();
+	const reverseString = reverseLetters.join("");
+
+    if (word === reverseString) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 /* 
   Add your pseudocode here
+    function declaration
+        if word is === to palindrome
+          return true 
+        else
+          return false 
 */
 
 /*
@@ -11,15 +25,15 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    if (require.main === module) {
+      // add your own custom tests in here
+      console.log("Expecting: true");
+      console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+      console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+      console.log("Expecting: false");
+      console.log("=>", isPalindrome("robot"));
+    }
 
-module.exports = isPalindrome;
+    module.exports = isPalindrome;
